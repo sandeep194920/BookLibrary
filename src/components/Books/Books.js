bd8bb3139ba48c900c26e4be05b48f5afcb004a3
sandeep194920 <sandeepamaranath@gmail.com>
2021-01-29 18:14:07 -0500

    getting books data from a file as an array

--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import Book from '../Book/Book'
 import classes from "./Books.module.scss";
+import { booksData } from '../../utils/data'
 
 function Books(props) {
 
     return (
         <>
             <div className={classes.books}>
-                <Book category="Love Fantacy" language="English" name="The Sacrifice" pages="220" price="30" />
+                {/* <Book category="Love Fantacy" language="English" name="The Sacrifice" pages="220" price="30" />
                 <Book category="Thriller Mystery" language="English" name="Innocent Criminal" pages="150" price="45" />
-                <Book category="Fiction" language="Spanish" name="Power of Belief" pages="300" price="50" />
+                <Book category="Fiction" language="Spanish" name="Power of Belief" pages="300" price="50" /> */}
+                {booksData.map(book => {
+                    return (
+                        <Book {...book} />
+                    )
+                })}
+
             </div>
         </>
     )
---
+++ src/utils/data.js
@@ -0,0 +1,49 @@
+
+
+// this file contains all the books (array of books) we render from
+
+export const booksData = [
+    {
+        name: 'The Sacrifice',
+        category: 'Love Fantacy',
+        language: 'English',
+        price: "30",
+        pages: "220"
+    },
+    {
+        name: 'Innocent Criminal',
+        category: 'Thriller Mystery',
+        language: 'Spanish',
+        price: "45",
+        pages: "250"
+    },
+    {
+        name: 'Power of belief',
+        category: 'Fiction',
+        language: 'French',
+        price: "50",
+        pages: "270"
+    },
+    {
+        name: 'Self respect',
+        category: 'Fiction',
+        language: 'English',
+        price: "25",
+        pages: "170"
+    },
+    {
+        name: 'A Sweet Pain',
+        category: 'Love Fantacy',
+        language: 'German',
+        price: "43",
+        pages: "120"
+    },
+    {
+        name: 'Invisible weapon',
+        category: 'Thriller Mystery',
+        language: 'English',
+        price: "35",
+        pages: "260"
+    },
+]
+
