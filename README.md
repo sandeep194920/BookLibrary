65b3a907eae26ebb6c1b3fbd410d17ee648747cb
sandeep194920 <sandeepamaranath@gmail.com>
2024-03-01 09:13:47 -0500

    build added

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Star Book Paradise
 
 Hosted at https://elegant-bardeen-ee256d.netlify.app/
+Hosted on Amazon S3 - http://mybookstoreapp.s3-website.ca-central-1.amazonaws.com/
 
 This is a simple CRUD responsive web app built with react-redux library. SASS has also been used in this. We have 3 type of books: Fiction, Thriller Mystery and Love Fantacy. Each category book gets its own theme, look and feel.
 You can add a book, remove it or even updated the existing book. Once the page is refreshed you lose the data and see 9 books which is hardcoded. There is no local storage used here because of which it remembers the data only until the browser refresh. There is no back-end used in this.
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,27 +1,36 @@
 import React from 'react'
-import logo from "../../images/arrow-star.png"
-import classes from "./Header.module.scss"
-import { connect } from 'react-redux';
-import * as actionCreators from '../../redux/actions/';
+import logo from '../../images/arrow-star.png'
+import classes from './Header.module.scss'
+import { connect } from 'react-redux'
+import * as actionCreators from '../../redux/actions/'
 
 function Header(props) {
-    return (
-        <header className={classes.header}>
-            <img className={classes.header__logo} src={logo} alt="Logo" />
-            <h1 className={classes.header__text}>Star Book Paradise  </h1>
-            {/* the css for addicon doesn't apply if className is given. Hence changing it to class */}
-            <ion-icon class={classes.header__addicon} onClick={() => props.onAddBookMode(true)} name="add-circle-outline"></ion-icon>
-            <button className={classes.header__btn} onClick={() => props.onAddBookMode(true)}>Add New Book</button>
-        </header>
-    )
+  return (
+    <header className={classes.header}>
+      <img className={classes.header__logo} src={logo} alt="Logo" />
+      <h1 className={classes.header__text}>Star Book Paradize </h1>
+      {/* the css for addicon doesn't apply if className is given. Hence changing it to class */}
+      <ion-icon
+        class={classes.header__addicon}
+        onClick={() => props.onAddBookMode(true)}
+        name="add-circle-outline"
+      ></ion-icon>
+      <button
+        className={classes.header__btn}
+        onClick={() => props.onAddBookMode(true)}
+      >
+        Add Book
+      </button>
+    </header>
+  )
 }
 
 // for dispatching the actions into the store
-const mapDispatchToProps = dispatch => {
-    return {
-        onAddBookMode: (addMode) => dispatch(actionCreators.addBookMode(addMode)),
-    }
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onAddBookMode: (addMode) => dispatch(actionCreators.addBookMode(addMode)),
+  }
 }
 
 // connect is a higher order function that connects this component to store
-export default connect(null, mapDispatchToProps)(Header);
+export default connect(null, mapDispatchToProps)(Header)
